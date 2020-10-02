59446d910fb076764ffa2eb5fedc6347d86042fd
HelloZXB <Hello16369@163.com>
2020-10-02 16:07:16 +0800

    数值类型

---
+++ 02-variaBles&dataTypes/03-数值类型.ts
@@ -0,0 +1,13 @@
+// 整数
+let size: number = 19;
+
+// 小数
+let score: number = 99.9;
+console.log(size, score);
+
+// 正数
+let positive: number = +2000;
+
+// 负数
+let negative: number = -4000;
+console.log(positive, negative);
