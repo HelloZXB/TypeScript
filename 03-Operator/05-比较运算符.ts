ee4592795c770fcc645e9a7408024ab232757a0a
HelloZXB <Hello16369@163.com>
2020-10-02 16:53:11 +0800

    比较运算符

---
+++ 03-Operator/05-比较运算符.ts
@@ -0,0 +1,21 @@
+// 大于
+console.log(1 > 2); // false
+
+// 大于等于
+console.log(1 >= 2); // false
+
+// 小于
+console.log(1 < 2); // true
+
+// 小于等于
+console.log(1 <= 2); // true
+
+// 相等
+let num1: number = 1;
+let num2: number = 2;
+console.log(num1 === num2); // false
+
+// 不相等
+let num3: number = 1;
+let num4: number = 2;
+console.log(num3 !== num4); // true
