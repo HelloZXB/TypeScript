d56f5b6eb1a5532857b5f774b3ed872bd59d9be9
HelloZXB <Hello16369@163.com>
2020-10-02 16:35:52 +0800

    加号的说明

---
+++ 03-Operator/02-加号的说明.ts
@@ -0,0 +1,16 @@
+console.log('朱晓' + '冰'); // 朱晓冰
+
+console.log(1 + '2'); // 12
+
+console.log('2' + 4); // 24
+
+console.log(2 - +'1'); // +'1'表示将 '1'(string) => 1(number);
+
+let age: number = +'18';
+console.log(age); // 18
+
+// console.log(2 - '1'); // error
+
+// console.log(2 * '2'); // error
+
+// console.log(2 / '2'); // error
