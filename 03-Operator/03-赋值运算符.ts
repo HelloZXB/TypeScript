9b4353fd2a45e3c053e184b45946de685dabbd64
HelloZXB <Hello16369@163.com>
2020-10-02 16:41:29 +0800

    赋值运算符

---
+++ 03-Operator/03-赋值运算符.ts
@@ -0,0 +1,19 @@
+// let age: number = 18;
+// age = age + 1;
+// console.log(age); // 19
+
+// let age: number = 18;
+// age += 19;
+// console.log(age); // 19
+
+// let age: number = 18;
+// age -= 19;
+// console.log(age); // -1
+
+// let age: number = 18;
+// age *= 19;
+// console.log(age); // 342
+
+let age: number = 18;
+age /= 19;
+console.log(age); // 0.9473684210526315
