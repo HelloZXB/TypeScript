32cd71c043c5757894c939f6a2b5f0ae7e6f67c5
HelloZXB <Hello16369@163.com>
2020-10-03 13:15:12 +0800

    三元练习

---
+++ 04-Statement/05-三元练习.ts
@@ -0,0 +1,9 @@
+let age: number = 18;
+// if (age < 18) {
+//     console.log('回家写作业');
+// } else {
+//     console.log('回家吃鸡')
+// }
+
+let result: string = age <  18 ? '回家写作业' : '回家吃鸡';
+console.log(result);
