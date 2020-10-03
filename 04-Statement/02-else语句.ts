b33d9c7b8e1c01fe6a43b2bf722ebee4b0341302
HelloZXB <Hello16369@163.com>
2020-10-03 13:03:04 +0800

    else语句

---
+++ 04-Statement/02-else语句.ts
@@ -0,0 +1,9 @@
+// 变量isVip表示: 是否为会员
+let isVip: boolean = true;
+
+// 需求: 如果是会员, 就打印 '看完整电影', 如果不是会员, 就打印 '您不是vip,只能试看5分钟'
+if (isVip) {
+    console.log('看完整电影')
+} else {
+    console.log('您不是vip,只能试看5分钟');
+}
